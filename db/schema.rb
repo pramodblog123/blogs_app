793c39907c885a9dfbef9e364e9aafcefcd6aebf
Pramod <pkpandit151197@gmail.com>
2026-02-16 18:43:47 +0530

    database migrated sqllite to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2026_02_13_184034) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "category"
